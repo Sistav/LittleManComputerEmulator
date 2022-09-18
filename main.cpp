4c389731f2ce310e8537c3912eee855c01a8e801
Georgios Dialynas-Vatsis <47365973+Sistav@users.noreply.github.com>
2022-09-18 01:43:35 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+int direct(int opcode){
+    switch (opcode) {
+//      HLT
+        case 0:
+
+            break;
+//      ADD
+        case 1:
+
+            break;
+//      SUB
+        case 2:
+
+            break;
+//      STA
+        case 3:
+
+        break;
+//      LDA
+        case 5:
+
+            break;
+//      BRA
+        case 6:
+
+            break;
+//      BRZ
+        case 7:
+
+            break;
+//      BRP
+        case 8:
+
+            break;
+
+    }
+}
+
+void HLT(){
+
+}
+void ADD(int integer){
+
+}
+
+void SUB(int integer){
+
+}
+
+void STA(int integer){
+
+}
+
+void LDA(int integer){
+
+}
+
+void BRA(int integer){
+
+}
+
+void BRZ(int integer){
+
+}
+void BRP(int integer){
+
+}
+
+int main() {
+    int programCounter;
+    int instructionRegister;
+    int addressRegister;
+    bool negativeFlag;
+    int ram = {100,};
+
+}
+
